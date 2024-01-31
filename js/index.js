7b7e77c45fceb035267bd3661499dd6b2d7d00b7
julio22a2r <00163922@uca.edu.sv>
2024-01-31 15:34:52 -0600

    ADD: hamburguer menu y fondosFaqSection

--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
-const boxes= document.querySelectorAll('.box');
+const boxes = document.querySelectorAll('.box');
 window.addEventListener('scroll', checkBoxes);
 
 checkBoxes();
 
-function checkBoxes(){
-    const triggerBottom = window.innerHeight / 5 *4;
+function checkBoxes() {
+  const triggerBottom = window.innerHeight / 5 * 4;
 
-    boxes.forEach((box) => {
-        const boxTop = box.getBoundingClientRect().top;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
 
-        if(boxTop < triggerBottom){
-            box.classList.add('show');
-        }else{
-            box.classList.remove('show');
-        }
-    
-    });
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
 }
-
+document.addEventListener('DOMContentLoaded', function() {
+    var hamburgerMenu = document.querySelector('.hamburger-menu');
+    var menuItems = document.querySelector('.menu-items');
+  
+    hamburgerMenu.addEventListener('click', function(event) {
+      event.stopPropagation(); // Prevent this click from triggering the document's click event
+      if (menuItems.style.display === 'none' || menuItems.style.display === '') {
+        menuItems.style.display = 'flex';
+      } else {
+        menuItems.style.display = 'none';
+      }
+    });
+  
+    document.addEventListener('click', function() {
+      menuItems.style.display = 'none';
+    });
+  });
