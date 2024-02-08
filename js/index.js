ae9088a4341499f7bf516444ee864323baddaee1
julio22ar <00163922@uca.edu.sv>
2024-02-08 09:28:46 -0600

    MODIFIED: version Julio Mobile =

--- js/index.js
+++ js/index.js
@@ -1,20 +1,42 @@
-const boxes= document.querySelectorAll('.box');
+const boxes = document.querySelectorAll('.box');
 window.addEventListener('scroll', checkBoxes);
 
 checkBoxes();
 
-function checkBoxes(){
-    const triggerBottom = window.innerHeight / 5 *4;
+function checkBoxes() {
+  const triggerBottom = window.innerHeight / 5 * 4;
 
-    boxes.forEach((box) => {
-        const boxTop = box.getBoundingClientRect().top;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
 
-        if(boxTop < triggerBottom){
-            box.classList.add('show');
-        }else{
-            box.classList.remove('show');
-        }
-    
-    });
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
 }
 
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelector('.menu-items');
+
+// Set the initial state of menuItems.style.left
+menuItems.style.left = '-100%';
+
+const toggleMenu = () => {
+    menuItems.style.left = menuItems.style.left === '-100%' ? '0' : '-100%';
+}
+
+hamburgerMenu.addEventListener('click', (event) => {
+    event.stopPropagation(); // Prevent the event from bubbling up to the document
+    toggleMenu();
+});
+
+document.addEventListener('click', (event) => {
+    const isClickInsideMenu = menu.contains(event.target);
+
+    if (!isClickInsideMenu && menuItems.style.left === '0') {
+        toggleMenu();
+    }
+});
